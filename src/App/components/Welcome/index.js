0493ec34980317c75fc17860640d613dad211f37
Will Friebel <will.friebel@gmail.com>
2018-11-25 16:05:58 -0800

    Host logo image using CDN

--- src/App/components/Welcome/index.js
+++ src/App/components/Welcome/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './styles.scss';
 import NextPageButton from '../NextPageButton';
-import Logo from './images/logo.png';
+
+const logoURL = 'https://i.imgur.com/FyLIFAu.png';
 
 export default class Welcome extends Component {
   render() {
     return (
       <div className="Welcome">
-        <img className="Welcome__logo" src={Logo} alt="Keepin it Feel"/>
+        <img className="Welcome__logo" src={logoURL} alt="Keepin it Feel"/>
         <p className="Welcome__subtitle">This reflection exercise is for understanding your emotions and how you react in different situations. As you answer the questions, our hope is that you gain a better understanding of your emotions in response to the world around you. This exercise works best if you’re currently experiencing the emotions or have recently experienced them.</p>
         <NextPageButton 
           navigatePage={this.props.navigatePage}
